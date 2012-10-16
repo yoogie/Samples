ad4980d2e4c6e2dbbb17f16bcd9c05f4c47471c7
yoogie <bjorn@bitsstockholm.se>
2012-10-16 13:37:16 +0200

    Added empty STL examples

---
+++ stl/stl.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <typeinfo>
+
+
+void useMap()
+{
+}
+
+void findInMultiMap()
+{
+}
+
+void remove()
+{
+}
+
+void freeMemory()
+{
+}
+
+void itterate()
+{
+}
+
+void swap()
+{
+}
+   
+void benchmarkInsert()
+{
+}
+
+void benchmarkLookup()
+{
+}
+
+void benchmarkDelete()
+{
+}
+
+int main()
+{
+    useMap();
+    findInMultiMap();
+
+    remove();
+    freeMemory();
+
+    itterate();
+
+    swap();
+   
+    benchmarkInsert();
+    benchmarkLookup();
+    benchmarkDelete();
+
+    return 1;
+}
