98048dbbca833c109010eed6155942f40bfae866
yoogie <bjorn@bitsstockholm.se>
2012-12-11 11:08:35 +0100

    More draft code for stringformating
    Added some first basic examples for string formating.

--- string_formating/string_formating.cpp
+++ string_formating/string_formating.cpp
@@ -1,51 +1,65 @@
 #include <string>
 #include <boost\format.hpp>
 
-class Perf
-{
-public:
-    Perf() : m_whatToMeassure()
-    {
-        QueryPerformanceFrequency(&m_cpuFreq);
-        m_start.HighPart = m_start.LowPart = 0;
-        m_stop.HighPart = m_stop.LowPart = 0;
-    }
-    void Start(const string& whatToMeassure) 
-    {
-        m_whatToMeassure = whatToMeassure;
-        QueryPerformanceCounter(&m_start);
-    }
-    void Stop()
-    {
-        QueryPerformanceCounter(&m_stop);
-    }
-    void Print()
-    {        
-        double differance = static_cast<double>(m_stop.QuadPart - m_start.QuadPart);
-        cout << m_whatToMeassure << ": " << 1000 * (differance / m_cpuFreq.QuadPart) << " ms" << endl;
-    }
+using namespace std;
+#define UseEndl 1
 
-private:
-    string m_whatToMeassure;
-    LARGE_INTEGER m_cpuFreq;
-    LARGE_INTEGER m_start;
-    LARGE_INTEGER m_stop;
-};
 
 void use_printf_simple()
 {
+    printf("Hello world\n");
 }
 
 void use_cout_simple()
 {
+#ifdef UseEndl
+    cout << "Hello world" << endl;
+#else
+    cout << "Hello world\n";
+#endif
 }
 
 void use_vsprintf_simple()
 {
+    const size_t buffSize = 15;
+    char buff[buffSize];
+    memset(buff, 0, buffSize);
+    vsnprintf(buff, 13, "Hello world\n", NULL);
+    cout << buff << endl;
+
+    memset(buff, 0, buffSize);
+    vsnprintf(buff, 5, "Hello world\n", NULL);
+    cout << buff << endl;
+
+    ////It is till possible to write more than there is room for in the buffer
+    ////this would corrupt the stack
+    
+    //memset(buff, 0, buffSize);
+    //vsnprintf(buff, 20, "Hello world, more than 15 long\n", NULL);
+    //cout << buff << endl;
+
+    //Safe version _s
+    memset(buff, 0, buffSize);
+    vsnprintf_s(buff, buffSize, 12, "Hello world\n", NULL);
+    cout << buff << endl;
+
+    memset(buff, 0, buffSize);
+    vsnprintf_s(buff, buffSize, 5, "Hello world\n", NULL);
+    cout << buff << endl;
+
+    ////Trying to print more than there is roome for in the buffer will give a 
+    ////debug assertion.
+
+    //memset(buff, 0, buffSize);
+    //vsnprintf_s(buff, buffSize, 20, "Hello world, more than 15 long\n", NULL);
+    //cout << buff << endl;
 }
 
 void use_boostFormat_simple()
 {
+    //TODO Make proper examples for boost::format
+    boost::format fmt("Hello world\n", locale::classic());
+    cout << fmt.str() << endl;
 }
 
 
@@ -67,15 +81,15 @@ void use_boostFormat_complex()
 
 int main()
 {
-    use_printf_simple(); std::cout << std::endl;
-    use_cout_simple(); std::cout << std::endl;
-    use_vsprintf_simple(); std::cout << std::endl;
-    use_boostFormat_simple(); std::cout << std::endl;
+    use_printf_simple(); cout << endl;
+    use_cout_simple(); cout << endl;
+    use_vsprintf_simple(); cout << endl;
+    use_boostFormat_simple(); cout << endl;
 
-    use_printf_complex(); std::cout << std::endl;
-    use_cout_complex(); std::cout << std::endl;
-    use_vsprintf_complex(); std::cout << std::endl;
-    use_boostFormat_complex(); std::cout << std::endl;
+    use_printf_complex(); cout << endl;
+    use_cout_complex(); cout << endl;
+    use_vsprintf_complex(); cout << endl;
+    use_boostFormat_complex(); cout << endl;
 
     return 1;
 }
