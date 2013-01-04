cb32fa34f8a426826ed6237d7ce0153bb05a7991
yoogie <bjorn@bitsstockholm.se>
2013-01-04 09:41:05 +0100

    Added more sorted vector/sets
    Added example for using set with sorter help function.
    Added examples for using a sorted vector.

--- stl/SortedData.cpp
+++ stl/SortedData.cpp
@@ -21,12 +21,26 @@ private:
     Sortable();
 };
 
+class NonSortable
+{
+public:
+    NonSortable(int id) : mId(id) {}
+    int mId;
+private:
+    NonSortable();
+};
+
+bool comparefunction(const NonSortable& lhs, const NonSortable& rhs)
+{
+    return lhs.mId < rhs.mId;
+}
+
+typedef bool(*Comparator)(const NonSortable& lhs, const NonSortable& rhs);
 
 void useASet()
 {
     //Insert some random ints into a set.
     std::set<int> ints;
-
     ints.insert(3);
     ints.insert(13);
     ints.insert(1);
@@ -40,7 +54,6 @@ void useASet()
 
     //Insert some chars into a set.
     std::set<char> chars;
-
     chars.insert('b');
     chars.insert('2');
     chars.insert('w');
@@ -54,8 +67,9 @@ void useASet()
     }
 
     //Insert some Sortable objects into a set.
+    //It is not possible to insert objects that 
+    //does not have  a operator< implemented.
     std::set<Sortable> sorts;
-
     sorts.insert(Sortable(3));
     sorts.insert(Sortable(13));
     sorts.insert(Sortable(1));
@@ -66,10 +80,101 @@ void useASet()
     {
         std::cout << it->mId << std::endl;
     }
+
+    //Insert some NonSortable objects into a set
+    //using a local sorter function
+    //std::set<NonSortable, bool(*)(const NonSortable& lhs, const NonSortable& rhs)> nonSorts(&comparefunction);
+    std::set<NonSortable, Comparator> nonSorts(&comparefunction);
+    nonSorts.insert(NonSortable(3));
+    nonSorts.insert(NonSortable(13));
+    nonSorts.insert(NonSortable(1));
+    nonSorts.insert(NonSortable(4));
+
+    std::cout << "NonSortable set after insertion: " << std::endl;
+    for(std::set<NonSortable, Comparator>::const_iterator it = nonSorts.begin(); it != nonSorts.end(); ++it)
+    {
+        std::cout << it->mId << std::endl;
+    }
+
+    //Insert some more ints into a set
+    std::set<int> moreInts;
+    moreInts.insert(3);
+    moreInts.insert(13);
+    moreInts.insert(1);
+    moreInts.insert(4);
+    moreInts.insert(4);
+    moreInts.insert(4);
+    moreInts.insert(4);
+
+    //Since a set contains unique objects
+    std::cout << "Int set after insertion: " << std::endl;
+    for(std::set<int>::const_iterator it = ints.begin(); it != ints.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+    
+    std::pair<std::set<int>::iterator, bool> res = moreInts.insert(4);
+    std::cout << "res.bool = " << res.second << " after trying to inser 4 for the 5th time." << std::endl;
 }
 
 void sortedVector()
 {
+    using std::sort;
+    using std::unique;
+
+    std::vector<int> ints;
+    ints.push_back(3);
+    ints.push_back(13);
+    ints.push_back(1);
+    ints.push_back(4);
+
+    std::cout << "Int vector after insertion: " << std::endl;
+    for(std::vector<int>::const_iterator it = ints.begin(); it != ints.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    sort(ints.begin(), ints.end());
+    
+    std::cout << "Int vector after sort: " << std::endl;
+    for(std::vector<int>::const_iterator it = ints.begin(); it != ints.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    //Insert some more ints into a vector
+    std::vector<int> moreInts;
+    moreInts.push_back(3);
+    moreInts.push_back(13);    
+    moreInts.push_back(4);
+    moreInts.push_back(1);
+    moreInts.push_back(4);
+    moreInts.push_back(4);
+    moreInts.push_back(4);
+
+    std::cout << "Int vector after insertion: " << std::endl;
+    for(std::vector<int>::const_iterator it = moreInts.begin(); it != moreInts.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    //To have the vector similar to the set, sort and remove copies
+    sort(moreInts.begin(), moreInts.end()); //sort before making unique since std::unique only checks elements next to each other
+    std::vector<int>::iterator newEndIt = unique(moreInts.begin(), moreInts.end());
+    
+    std::cout << "Int vector after sorting and removing copies: " << std::endl;
+    for(std::vector<int>::const_iterator it = moreInts.begin(); it != moreInts.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    //Since std::unique does not invalidate elements after the last unique element we need to manualy shrink the vector
+    moreInts.resize(newEndIt - moreInts.begin());
+    std::cout << "Int vector after resize: " << std::endl;
+    for(std::vector<int>::const_iterator it = moreInts.begin(); it != moreInts.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
 }
 
 void compareSetAndSortedVector()
